683c91a804fbf2b4441cf508c1e8d399e64d1178
BassTheCoder <patryk.nowak.hello@gmail.com>
2022-02-12 17:05:04 +0100

    delete redundant path

--- djangosite/urls.py
+++ djangosite/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('notes.urls')),
-    path('accounts/', include('registration.backends.simple.urls')),
 ]
