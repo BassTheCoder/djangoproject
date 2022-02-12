2144384dfadfcec1654600875ba13959d87e7dbf
BassTheCoder <patryk.nowak.hello@gmail.com>
2022-02-12 17:03:40 +0100

    finish

--- djangosite/settings.py
+++ djangosite/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'notes',
     'material',
-    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
