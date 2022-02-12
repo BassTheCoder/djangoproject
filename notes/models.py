0fcad523974d0db7f8307b3a27e591c18aac9b9c
BassTheCoder <patryk.nowak.hello@gmail.com>
2022-02-12 10:40:42 +0100

    init repo

---
+++ notes/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.utils import timezone
+
+class Note(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    date_created = models.DateTimeField(default=timezone.now)
